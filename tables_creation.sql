accbf3abeedf6e269a4dcfe12287822d78336a4b
Sergio Yalux <randaledomman1@gmail.com>
2022-06-24 00:30:38 -0300

    feat(server): separate sql tables and functions in different files

--- tables_creation.sql
+++ tables_creation.sql
@@ -2,10 +2,10 @@
 CREATE TABLE Client(
 	pk_client_id SERIAL PRIMARY KEY,
 	nickname VARCHAR(40) UNIQUE NOT NULL,
-  fullname VARCHAR(40) NOT NULL,
+  	fullname VARCHAR(40) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	hash VARCHAR(128) NOT NULL,
-  salt VARCHAR(32) NOT NULL,
+  	salt VARCHAR(32) NOT NULL,
 	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -35,63 +35,3 @@ CREATE TABLE TipComment(
   FOREIGN KEY (fk_from_tip_id) REFERENCES TIP (pk_tip_id),
   FOREIGN KEY (fk_to_tip_id) REFERENCES TIP (pk_tip_id)
 );
-
-/* Functions Definition */
-CREATE OR REPLACE FUNCTION fn_GetTipFromClient (cid INTEGER, tid INTEGER)
-RETURNS TABLE (
-	client_id INTEGER,
-	tip_id INTEGER,
-	body VARCHAR(240),
-  fullname VARCHAR(40),
-	nickname VARCHAR(40),
-	created TIMESTAMP
-)
-LANGUAGE plpgsql
-AS
-$$
-BEGIN
-	RETURN QUERY
-		SELECT 
-			C.pk_client_id AS client_id,
-			T.pk_tip_id AS tip_id,
-			T.body AS body,
-      C.fullname AS fullname,
-			C.nickname AS nickname,
-			T.created AS created
-		FROM Tip T
-		JOIN Client C 
-			ON T.fk_client_id = C.pk_client_id
-		WHERE 
-			T.pk_tip_id = tid 
-			AND
-			C.pk_client_id = cid;	
-END;
-$$;
-
-CREATE OR REPLACE FUNCTION fn_GetAllTipsFromClient(cid INTEGER)
-RETURNS TABLE (
-	client_id INTEGER,
-	tip_id INTEGER,
-	body VARCHAR(240),
-  fullname VARCHAR(40),
-	nickname VARCHAR(40),
-	created TIMESTAMP
-)
-LANGUAGE plpgsql
-AS
-$$
-BEGIN
-	RETURN QUERY
-		SELECT
-			C.pk_client_id AS client_id,
-			T.pk_tip_id AS tip_id,
-			T.body AS body,
-      C.fullname AS fullname,
-			C.nickname AS nickname,
-			T.created AS created
-		FROM Tip T
-		JOIN Client C
-			ON T.fk_client_id = C.pk_client_id
-		WHERE C.pk_client_id = cid;
-END;
-$$;
