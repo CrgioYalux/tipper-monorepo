21cf0c7ff14e13235aa247e3d510004f0e3259ef
Sergio Yalux <randaledomman1@gmail.com>
2022-08-10 02:06:30 -0300

    chore: 🧹

--- client/src/mockdata/comments.js
+++ client/src/mockdata/comments.js
@@ -1,36 +1,32 @@
-// COMMENTS
-
-const comment1 = {
-  nickname: 'tyrwellick',
-  fullname: 'Tyrell Wellick',
-  body: 'bonsoir Elliot',
-  created: new Date(),
-  likes: 2,
-  dislikes: 1,
-  comments: 0,
-  tip_id: 12
-};
-
-const comment2 = {
-  nickname: 'angymoss',
-  fullname: 'Angela Moss',
-  body: 'R u coming to my bday?',
-  created: new Date(),
-  likes: 2,
-  dislikes: 1,
-  comments: 0,
-  tip_id: 13
-};
-
-const comment3 = {
-  nickname: 'angymoss',
-  fullname: 'Angela Moss',
-  body: '!@#$%^&*',
-  created: new Date(),
-  likes: 10,
-  dislikes: 6,
-  comments: 0,
-  tip_id: 14
-};
-
-export const mock_comments = [comment1, comment2, comment3];
+export const mock_comments = [
+  {
+    nickname: 'tyrwellick',
+    fullname: 'Tyrell Wellick',
+    body: 'bonsoir Elliot',
+    created: new Date(),
+    likes: 2,
+    dislikes: 1,
+    comments: 0,
+    tip_id: 12
+  },
+  {
+    nickname: 'angymoss',
+    fullname: 'Angela Moss',
+    body: 'R u coming to my bday?',
+    created: new Date(),
+    likes: 2,
+    dislikes: 1,
+    comments: 0,
+    tip_id: 13
+  },
+  {
+    nickname: 'angymoss',
+    fullname: 'Angela Moss',
+    body: '!@#$%^&*',
+    created: new Date(),
+    likes: 10,
+    dislikes: 6,
+    comments: 0,
+    tip_id: 14
+  } 
+];
--- client/src/mockdata/users.js
+++ client/src/mockdata/users.js
@@ -1,11 +1,10 @@
-const user1 = {
-  nickname: 'azul1_',
-  password: '123456789a'
-};
-
-const user2 = {
-  nickname: '_yellow1_',
-  password: 'aReallyStrongPassword'
-};
-
-export const mock_users = [user1, user2];
+export const mock_users = [
+  {
+    nickname: 'azul1_',
+    password: '123456789a'
+  },
+  {
+    nickname: '_yellow1_',
+    password: 'aReallyStrongPassword'
+  }
+];
